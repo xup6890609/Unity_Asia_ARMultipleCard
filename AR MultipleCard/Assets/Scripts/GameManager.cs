b2c9a8231ffdb527e8542f667092aabf274cdb9e
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2020-11-03 12:06:23 +0800

    加入旋轉、縮放程式參數

--- AR MultipleCard/Assets/Scripts/GameManager.cs
+++ AR MultipleCard/Assets/Scripts/GameManager.cs
@@ -1,8 +1,35 @@
 ﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
-  public Transform Sheep;
-  public Transform Duck;
+{  // 建立欄位標題 
+    [Header ("綿羊")]
+     public Transform Sheep;
 
- }
+    [Header ("鴨子")]
+    public Transform Duck;
+
+    [Header ("虛擬搖桿")]
+    public FixedJoystick Joystick;
+
+    [Header ("旋轉速度"),Range(0.1f,10f)]
+    public float turn=1.5f;
+    [Header("縮放"), Range(0f, 5f)]
+    public float size = 0.01f;
+
+    // 開始事件(初始設定)
+    private void Start()
+    {
+        print ("開事件執行");  
+    }
+    // 更新事件(影格) //控制旋轉、移動、縮放或玩家輸入
+    private void Update()
+    {   print ("更新事件");
+        print (Joystick.Vertical);
+
+        Sheep.Rotate (0, Joystick.Horizontal*turn, 0);
+        Duck.Rotate (0, Joystick.Horizontal*turn, 0);
+
+        Sheep.localScale+=new Vector3(1,1,1)*Joystick.Vertical*size;
+
+    }
+}

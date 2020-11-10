5fb74fd202836ea14cf081e211d77c7c655daea6
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2020-11-10 10:45:41 +0800

    新增動畫控制

--- AR MultipleCard/Assets/Scripts/GameManager.cs
+++ AR MultipleCard/Assets/Scripts/GameManager.cs
@@ -13,9 +13,15 @@ public class GameManager : MonoBehaviour
 
     [Header ("旋轉速度"),Range(0.1f,10f)]
     public float turn=1.5f;
+
     [Header("縮放"), Range(0f, 5f)]
     public float size = 0.01f;
 
+    [Header("綿羊動畫元件")]
+    public Animator aniSheep;
+
+    [Header("鴨子動畫元件")]
+    public Animator aniDuck;
     // 開始事件(初始設定)
     private void Start()
     {
@@ -23,13 +29,27 @@ public class GameManager : MonoBehaviour
     }
     // 更新事件(影格) //控制旋轉、移動、縮放或玩家輸入
     private void Update()
-    {   print ("更新事件");
-        print (Joystick.Vertical);
+    { print("更新事件");
+        print(Joystick.Vertical);
 
-        Sheep.Rotate (0, Joystick.Horizontal*turn, 0);
-        Duck.Rotate (0, Joystick.Horizontal*turn, 0);
+        Sheep.Rotate(0, Joystick.Horizontal * turn, 0);
+        Duck.Rotate(0, Joystick.Horizontal * turn, 0);
 
-        Sheep.localScale+=new Vector3(1,1,1)*Joystick.Vertical*size;
+        Sheep.localScale += new Vector3(1, 1, 1) * Joystick.Vertical * size;
+    }
+        //public void Walk()
+        //{
+        // print("走路");
+        //aniSheep.SetTrigger("走路 觸發");
+        //aniDuck.SetTrigger("走路 觸發");
+        public void PlayAnimation(string aniName)
+    {   
+        print(aniName);
+        aniSheep.SetTrigger(aniName);
+        aniDuck.SetTrigger(aniName);
 
     }
-}
+         
+    }
+        
+
